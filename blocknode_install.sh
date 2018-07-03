1e7f70998504267641b5c97e35a42c1e501a868a
Zoldur <36329578+zoldur@users.noreply.github.com>
2018-07-03 17:25:06 +0100

    Update blocknode_install.sh

--- blocknode_install.sh
+++ blocknode_install.sh
@@ -122,10 +122,6 @@ listenonion=0
 masternode=1
 externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
-addnode=207.148.88.26
-addnode=45.77.241.199
-addnode=149.28.174.100
-addnode=149.28.26.118
 EOF
 }
 
